8a1b7f43af8fb96c72caa7cf7aefc37a048f359f
Junaid Asghar <junaidasghar862@gmail.com>
2021-01-26 20:18:11 +0500

    Calculator App

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var input = document.querySelector("input");
 var btn = document.querySelector("button");
 function nmbr(num) {
-  input.value += num;
+  var opt = ["+", "-", "*", "/"];
+  var lastValue = input.value.slice(input.value.length - 1);
+  if (opt.indexOf(lastValue) !== -1) {
+    console.log(true);
+  } else {
+    console.log(false);
+    input.value += num;
+  }
 }
 
 function cls() {
